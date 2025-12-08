24e2f99ee5f0045617e0a6a3205d3675d4b2b40c
Youcef <youcefcopy@gmail.com>
2025-12-08 16:08:54 +0100

    My next plan | track # positions, count the difference. if it has 1, 3 or 5 that's a win

--- app.js
+++ app.js
@@ -2,5 +2,31 @@ console.log('js loaded');
 const container = document.querySelector('.container');
 const boxes = document.querySelectorAll('.box');
 
+let isX = true; // ! Should be Global, so the event listener wil noe reset it again
+
+
 console.log(container);
 console.log(boxes);
+
+container.addEventListener('click',assignBox);
+
+
+function assignBox(e) {
+    if (isX) {
+        e.target.textContent = 'x';
+        isX = false;
+    } else{
+        e.target.textContent = 'o';
+        isX = true;
+    }
+    console.log(e.target);
+}
+
+// The idea is that we need to track the position of the x or o using a formula of: 
+//  0 1 2
+//  3 4 5 
+//  6 7 8 
+
+function trackPosition() {
+    // check the textContent of that container if it's x than get all x positions
+}
